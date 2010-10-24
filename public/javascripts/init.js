b32daa5c9b04aef469440514a2a6693fec1c06d6
Jakub Hozak <jakub.hozak@gmail.com>
2010-10-24 19:00:17 +0200

    Jasmine. Card dragging and tappping.

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    game = new Game();
+    game.connect();
+      
+
+
   // $('img').click(function(event) {
   //     event.preventDefault();
   // });
@@ -12,7 +17,7 @@ $(document).ready(function() {
   $('.card').each(function(i) {
     img = $(this);
     // img.data('card-id');
-      img.draggable();
+      img.draggable( { snap: true, snapMode: 'inner' } );
       img.toggleClass('small', 500);
 
       img.dblclick(function () {
