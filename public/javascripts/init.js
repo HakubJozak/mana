ab35012b747c76af60977e8f2d21edee5e07f14d
Jakub Hozak <jakub.hozak@gmail.com>
2010-10-27 01:11:59 +0200

    graveyard beginnings

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,35 +1,50 @@
+function preventAll() {
+  labels = [ 'img', '#deck' ];
+
+  for (i in labels) {
+    $(labels[i]).click(function(event) {
+      event.preventDefault();
+    });
+
+    $(labels[i]).mousedown(function(event) {
+      event.preventDefault();
+    });
+  }
+}
+
+
 $(document).ready(function() {
 
     //game = new Game('ws://connect');
     //game.connect();
+
+  preventAll();
       
-
-
-  // $('img').click(function(event) {
+  // $('img.card').mousewheel(function(event) {
   //     event.preventDefault();
   // });
 
-  $('img.card').mousedown(function(event) {
-      event.preventDefault();
+  $('#graveyard').droppable({
+      hoverClass: 'selected',
+      drop: function(event,ui) {
+          console.log('drop');
+      }
   });
-
-  $(".card").tooltip({ 
-      delay: 1000,
-      showURL: false,
-      bodyHandler: function() { 
-          image = $("<img/>").attr("src", this.src)
-          return image; 
-      }, 
-});
 
   $('.card').each(function(i) {
     img = $(this);
-    // img.data('card-id');
+      // img.data('card-id');
       img.draggable( { snap: true, snapMode: 'inner' } );
-      img.toggleClass('small', 500);
+      img.draggable( "option", "zIndex", 2700 );
+      //img.toggleClass('small', 500);
 
       img.dblclick(function () {
-          $(this).toggleClass('tapped');
+        $(this).toggleClass('tapped');
       });
+
+      // img.mousewheel(function(event, delta) {
+      //   //$(this).toggleClass('small');
+      //   console.log(delta);
+      // });
   });
 });
