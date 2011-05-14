50e1ff6fa9b6d3a61c1e5d2cdee5dfb7a1dd8c72
Jakub Hozak <jakub.hozak@gmail.com>
2011-05-14 21:40:14 +0200

    many little tweaks

--- client/test/integration/hello_test.rb
+++ client/test/integration/hello_test.rb
@@ -1,15 +1,17 @@
 require File.expand_path File.join('client/test/test_helper')
 
+require_all 'app'
+
 class HelloTest < Test::Unit::TestCase
   include Capybara
-  Capybara.default_driver = :webkit
+  Capybara.default_driver = :selenium
 
   def setup
-    Capybara.app = Sinatra::Application.new
+    Capybara.app = Mana::Server.new
   end
 
   def test_hello
-    visit '/'
+    visit '/flow'
     assert_equal "hello world", page.body
   end
 
