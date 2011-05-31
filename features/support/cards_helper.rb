3ab2f0e7d7d0e7417c29b42522e7c620c23200a4
Michal Cichra <m@o2h.cz>
2011-05-31 17:26:27 +0200

    fixed dragging card into empty graveyard or exile

--- features/support/cards_helper.rb
+++ features/support/cards_helper.rb
@@ -1,14 +1,4 @@
 module CardsHelper
-  #attr_accessor :current_card
-  
-  def current_card
-    @card
-  end
-  
-  def current_card= card
-    @card = card
-  end
-  
   def my_hand
     find(selector_for('my hand'))
   end
