cc5b9ad93ac84845e17ecc39c70701be5f9f5109
Jakub Hozak <jakub.hozak@gmail.com>
2012-02-13 10:12:13 +0100

    threaded stuff

--- script/loop.rb
+++ script/loop.rb
@@ -6,23 +6,37 @@
 
 Bundler.require(:test)
 require 'capybara/dsl'
+require 'thread'
 
 
 include Capybara::DSL
 Capybara.app_host = 'http://localhost:8080'
 Capybara.current_driver = :webkit
 
-10.times do |i|
-  name = "Igra #{i}"
+#ready = Queue.new
+threads = []
 
-  visit '/games'
-  click_link 'Create'
-  fill_in 'Name', with: "#{name}"
-  click_button 'Create Game'
+3.times do |i|
+  threads << Thread.new do
+    puts '1'
+ #   ready.pop
+    name = "Igra Thread #{i}"
 
-  fill_in 'Cards', with: '15,Forest'
-  click_button "Join '#{name}'"
+    visit '/games'
+    puts '2'
+    click_link 'Create'
+    puts '3'
+    fill_in 'Name', with: "#{name}"
+    puts '4'
+    click_button 'Create Game'
+    puts '5'
+
+    fill_in 'Cards', with: '15,Forest'
+    click_button "Join '#{name}'"
+    puts "Started #{name}"
+  end
 end
 
-
+#ready.push(true)
+threads.each { |t| t.join }
 
