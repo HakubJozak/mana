a3a493ebe778bd112a4c26847b2a2ee4809629b7
Jakub Hozak <jakub.hozak@gmail.com>
2012-02-13 10:27:34 +0100

    tapping script almost done

--- script/loop.rb
+++ script/loop.rb
@@ -13,33 +13,37 @@ include Capybara::DSL
 Capybara.app_host = 'http://localhost:8080'
 Capybara.current_driver = :webkit
 
-#ready = Queue.new
 tasks = []
 
+begin
+  3.times do |i|
+    tasks << fork do
+      puts '1'
+      name = "Igra Thread #{i}"
 
-3.times do |i|
-  tasks << fork do
-    puts '1'
- #   ready.pop
-    name = "Igra Thread #{i}"
+      visit '/games'
+      puts '2'
+      click_link 'Create'
+      puts '3'
+      fill_in 'Name', with: "#{name}"
+      puts '4'
+      click_button 'Create Game'
+      puts '5'
 
-    visit '/games'
-    puts '2'
-    click_link 'Create'
-    puts '3'
-    fill_in 'Name', with: "#{name}"
-    puts '4'
-    click_button 'Create Game'
-    puts '5'
+      fill_in 'Cards', with: '15,Forest'
+      click_button "Join '#{name}'"
+      puts "Started #{name}"
 
-    fill_in 'Cards', with: '15,Forest'
-    click_button "Join '#{name}'"
-    puts "Started #{name}"
+    50.times do
+      sleep rand(4)
+      puts "Task #{i}: tapping card"
+
+      # page.execute_script("console.info('something');")
+      puts page.evaluate_script('Card.all.first().name()');
+      # tap  card
+      end
+    end
   end
+ensure
+  tasks.each { |t| Process.waitpid(t) }
 end
-
-tasks.each { |t| Process.waitpid(t) }
-
-#ready.push(true)
-# tasks.each { |t| t.join }
-
