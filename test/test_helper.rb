c7fbc90d2e0664cfefd4a74caa92d7f007646a5a
Jakub Hozak <jakub.hozak@gmail.com>
2014-05-08 13:57:08 +0200

    devise test integration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
+  include Devise::TestHelpers
 
-  # Add more helper methods to be used by all tests here...
+  def john
+    @john ||= User.create!(email: 'me@there.net', name: 'John', password: 'test1234')
+  end
 end
