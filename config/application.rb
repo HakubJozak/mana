b9cb848f060bf51ac7880523daefaefde37c501f
Jakub Hozak <jakub.hozak@gmail.com>
2014-07-13 20:02:09 +0200

    lib autload

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,9 @@ module Mana
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
     # config.autoload_paths += %W(#{config.root}/lib)
-    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     config.log_level = :debug
 
